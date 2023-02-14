a04a281e7393e8f38e3e02d1d29fa718727f73d6
exp <arzuakov@yahoo.com>
2023-02-14 23:25:34 +0300

    h14.02.2023

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-fs = require('fs')
+// fs = require('fs')
 
-const data = fs.readFileSync('./data.txt')
-console.log(data.toString());
+// const data = fs.readFileSync('./data.txt')
+// console.log(data.toString());
+console.log(global);
