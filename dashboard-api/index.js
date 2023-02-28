18e4060df4606b6051b3b04f79303f02b7bcc632
arzuakov@yahoo.com <arzuakov@yahoo.com>
2023-02-28 17:01:09 +0300

    w28.02.2023

--- dashboard-api/index.js
+++ dashboard-api/index.js
@@ -1,10 +1,24 @@
 import express from 'express'
+import { userRouter } from './users/users.js'
 
 const port = 8000
 const app = express()
 
+app.use((req, res, next) => {
+  console.log("Время ", Date.now());
+  next()
+})
+
 app.get('/hello', (req, res) => {
-  res.send('Hello!')
+  // res.send('Hello!')
+  throw new Error('/hello error')
+})
+
+app.use('/users', userRouter)
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(401).send(err.message)
 })
 
 app.listen(port, () => {
