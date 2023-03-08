f916da141d3c6d694479fece4ad90b3f5a097206
exp <arzuakov@yahoo.com>
2023-03-09 00:51:52 +0300

    h09.03.2023

--- dashboard-api/src/app.ts
+++ dashboard-api/src/app.ts
@@ -1,5 +1,7 @@
 import express, { Express } from 'express'
 import { Server } from 'http'
+import { ExeptionFilter } from '../errors/exeption.filter'
+import { ILogger } from './logger/logger.interface'
 import { LoggerService } from './logger/logger.service'
 import { UserController } from './users/user.contriller'
 
@@ -7,21 +9,25 @@ export class App {
   app: Express
   server: Server
   port: number
-  logger: LoggerService
+  logger: ILogger
   userCotroller: UserController
+  exeptionFilter: ExeptionFilter
 
-  constructor(logger: LoggerService, userCotroller: UserController) {
+  constructor(logger: ILogger, userCotroller: UserController, exeptionFilter: ExeptionFilter) {
     this.app = express()
     this.port = 8000
     this.logger = logger
     this.userCotroller = userCotroller
+    this.exeptionFilter = exeptionFilter
   }
 
   useRoutes() {
     this.app.use('/users', this.userCotroller.router)
   }
 
-  useExeptionFilters(){}
+  useExeptionFilters() {
+    this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter))
+  }
 
   public async init() {
     this.useRoutes()
--- dashboard-api/src/main.ts
+++ dashboard-api/src/main.ts
@@ -1,10 +1,11 @@
+import { ExeptionFilter } from "../errors/exeption.filter";
 import { App } from "./app";
 import { LoggerService } from "./logger/logger.service";
 import { UserController } from './users/user.contriller';
 
 async function bootstrap() {
   const logger = new LoggerService()
-  const app = new App(logger, new UserController(logger))
+  const app = new App(logger, new UserController(logger), new ExeptionFilter(logger))
   await app.init()
 }
 
