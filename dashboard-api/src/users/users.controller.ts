2538ea0118a37b2fe0e01092b885778eecbe3be1
arzuakov@yahoo.com <arzuakov@yahoo.com>
2023-03-21 09:53:54 +0300

    w21.03.2023

--- dashboard-api/src/users/users.controller.ts
+++ dashboard-api/src/users/users.controller.ts
@@ -8,15 +8,17 @@ import 'reflect-metadata';
 import { IUserController } from './users.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
-import { UsersService } from './users.service';
 import { ValidateMiddleware } from '../../common/validate.middleware';
 import { sign } from 'jsonwebtoken';
+import { IConfigService } from '../../config/config.service.interface';
+import { IUserService } from './users.service.interface';
 
 @injectable()
 export class UsersController extends BaseController implements IUserController {
   constructor(
     @inject(TYPES.ILogger) private loggerService: ILogger,
-    @inject(TYPES.UserService) private userService: UsersService
+    @inject(TYPES.UserService) private userService: IUserService,
+    @inject(TYPES.ConfigService) private configService: IConfigService
   ) {
     super(loggerService);
     this.binRoutes([
@@ -46,7 +48,9 @@ export class UsersController extends BaseController implements IUserController {
       return next(new HTTPError(401, 'ошибка авторизации', 'login'));
     }
 
-    this.ok(res, {});
+    const jwt = await this.signJWT(req.body.email, this.configService.get('SECRET'));
+
+    this.ok(res, { jwt });
   }
 
   async register(
@@ -61,5 +65,21 @@ export class UsersController extends BaseController implements IUserController {
     }
 
     this.ok(res, { id: result.id, email: result.email });
+  }
+
+  private signJWT(email: string, secret: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      sign(
+        { email, iat: Math.floor(Date.now() / 1000) },
+        secret,
+        { algorithm: 'HS256' },
+        (err, token) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(token as string);
+        }
+      );
+    });
   }
 }
