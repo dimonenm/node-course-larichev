87c76cfe66dae1222a6b2b09211771cb8969b935
arzuakov@yahoo.com <arzuakov@yahoo.com>
2023-03-21 17:49:48 +0300

    w21.03.2023

--- dashboard-api/common/auth.middleware.ts
+++ dashboard-api/common/auth.middleware.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 import { JwtPayload, verify } from 'jsonwebtoken';
 
 export class AuthMiddleware implements IMiddleware {
-  constructor(private secret: string) { }
+  constructor(private secret: string) {}
   execute(req: Request, res: Response, next: NextFunction): void {
     if (req.headers.authorization) {
       verify(req.headers.authorization.split(' ')[1], this.secret, (err, payload) => {
